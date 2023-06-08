f54729a26cf562e3c50f3c4328bc4cd7dbfad171
matheusjustino <matheus.justino@ccc.ufcg.edu.br>
2023-06-08 16:54:52 -0300

    creating notifications page

---
+++ src/pages/notifications/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const NotificationsPage: NextPage = () => {
+	return (
+		<header className="sticky px-2 py-4 border-b-2 font-semibold bg-white">
+			<h1 className="text-xl font-bold">Notifications</h1>
+		</header>
+	);
+};
+
+export default NotificationsPage;
